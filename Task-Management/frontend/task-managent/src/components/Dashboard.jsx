0e351953fe456a1b6c255988663e09b9b271ece9
Rathodray <rathodakeshkk2001@gmail.com>
2025-09-15 15:08:39 +0530

    header

--- Task-Management/frontend/task-managent/src/components/Dashboard.jsx
+++ Task-Management/frontend/task-managent/src/components/Dashboard.jsx
@@ -11,11 +11,10 @@ import AppliedFilters from "./AppliedFilters";
 
 const Dashboard = () => {
   const [selectedOption, setSelectedOption] = useState("Select cards");
-  // const [filters, setFilters] = useState(null);
   const [isFullscreen, setIsFullscreen] = useState(false);
   const [showUsers, setShowUsers] = useState(false);
   const [filters, setFilters] = useState(null);
-const [appliedFiltersList, setAppliedFiltersList] = useState([]);
+  const [appliedFiltersList, setAppliedFiltersList] = useState([]);
 
   const { theme } = useContext(ThemeContext);
 
@@ -24,40 +23,35 @@ const [appliedFiltersList, setAppliedFiltersList] = useState([]);
   };
 
   const handleExitFullscreen = () => {
-  setIsFullscreen(false);
-  setShowUsers(false);
-};
-
-useEffect(() => {
-  const handleEsc = (event) => {
-    if (event.key === "Escape") {
-      handleExitFullscreen();6
-    }
+    setIsFullscreen(false);
+    setShowUsers(false);
   };
 
-  window.addEventListener("keydown", handleEsc);
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === "Escape") {
+        handleExitFullscreen();
+      }
+    };
 
-  return () => {
-    window.removeEventListener("keydown", handleEsc);
-  };
-}, []);
+    window.addEventListener("keydown", handleEsc);
 
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
 
- 
   return (
-
-    <div style={{ backgroundColor: theme.dashboard, minHeight: "80vh" }}>
-
+    <div>
       {isFullscreen && (
         <div className="d-flex justify-content-between align-items-center bg-light p-2">
           <div className="d-flex align-items-center ms-5">
-          
             <button
               className="btn btn-success mt-2 rounded-pill px-4"
               onClick={() => setShowUsers(true)}
             >
               Start
-            </button> 
+            </button>
             {showUsers && <ProfileRow />}
           </div>
           <button
@@ -69,66 +63,62 @@ useEffect(() => {
         </div>
       )}
 
-    
       {!isFullscreen && (
         <div>
-          <Header/>
-        <div className="d-flex justify-content-between align-items-center mt-2 px-2">
-           <div className="d-flex align-items-center ms-1">
-          <div className="dropdown ms-1">
-            <button
-              className="btn dropdown-toggle border rounded-pill shadow-sm bg-white text-dark bg-transparent fs-7 fw-bold px-4 ms-1"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              {selectedOption}
-            </button>
-            <ul className="dropdown-menu">
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={() => handleSelect("All cards")}
-                >
-                  All cards
-                </button>
-              </li>
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={() => handleSelect("My cards")}
-                >
-                  My cards
-                </button>
-              </li>
-            </ul>
+          <Header />
+          <div className="pt-5">
+            <div className="d-flex justify-content-between align-items-center mt-4 px-2">
+              <div className="d-flex align-items-center ms-1">
+                <div className="dropdown ms-1">
+                  <button
+                    className="btn dropdown-toggle border rounded-pill shadow-sm bg-white text-dark bg-transparent fs-7 fw-bold px-4 ms-1"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {selectedOption}
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <button
+                        className="dropdown-item"
+                        onClick={() => handleSelect("All cards")}
+                      >
+                        All cards
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="dropdown-item"
+                        onClick={() => handleSelect("My cards")}
+                      >
+                        My cards
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+                <AppliedFilters
+                  appliedFiltersList={appliedFiltersList}
+                  onClear={() => {
+                    setFilters({ Department: [], Role: [], Priority: [] });
+                    setAppliedFiltersList([]);
+                  }}
+                />
+              </div>
+              <AddTask />
+              <StandupWizard setIsFullscreen={setIsFullscreen} />
+              <Filter
+                onFiltersChange={(raw, applied) => {
+                  setFilters(raw);
+                  setAppliedFiltersList(applied);
+                }}
+              />
+            </div>
           </div>
-           <AppliedFilters
-        appliedFiltersList={appliedFiltersList}
-        onClear={() => {
-          setFilters({ Department: [], Role: [], Priority: [] });
-          setAppliedFiltersList([]);
-        }}
-      />
-      </div>
-          <AddTask />
-
-          <StandupWizard setIsFullscreen={setIsFullscreen} />
-
-          <Filter onFiltersChange={(raw, applied) => {
-              setFilters(raw);
-              setAppliedFiltersList(applied);
-}} />
-
-        </div>
         </div>
       )}
 
-    
-      <div
-        className="m-3 p-3 border"
-        //style={{ minHeight: "90vh", backgroundColor: theme.card }}
-      >
+      <div className="m-3 p-3 border">
         <div
           className="p-2 text-white"
           style={{ backgroundColor: theme.header }}
@@ -140,8 +130,7 @@ useEffect(() => {
             readOnly
           />
         </div>
-
-        <KanbanBoard />
+        <KanbanBoard />    
       </div>
     </div>
   );
