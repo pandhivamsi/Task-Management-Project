97ea8f9d3258358d579fd4cf698088ca14f99fa1
pandhivamsi <pvamsi3010@gmail.com>
2025-09-21 18:42:26 +0530

    projects

--- Task-Management/backend/Tasker/src/main/java/com/tasker/controller/TaskerController.java
+++ Task-Management/backend/Tasker/src/main/java/com/tasker/controller/TaskerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.tasker.service.TaskerService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173/projects")
 public class TaskerController {
 	
 	@Autowired
